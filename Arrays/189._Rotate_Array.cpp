94f630bca3652df5e13442802036845784d5e0d7
Suraj Yadav <76870806+y-suraj@users.noreply.github.com>
2023-01-30 19:13:11 +0530

    Create 189._Rotate_Array.cpp

---
+++ Arrays/189._Rotate_Array.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        int n = nums.size();
+        if(k>n) k = k-n;
+        int start = abs(n-k);
+        vector<int> answer;
+
+        for(int i = start; i<n; i++){
+            answer.push_back(nums[i]);
+        }
+        for(int i = 0; i<start; i++){
+            answer.push_back(nums[i]);
+        }
+        for(int i=0; i<n; i++)
+            nums[i] = answer[i];
+    }
+};
