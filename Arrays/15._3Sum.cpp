9cb33e161ae889d0fbd2498c8dcd9a0d1d0ae184
Suraj Yadav <76870806+y-suraj@users.noreply.github.com>
2023-02-08 12:17:50 +0530

    Create 15._3Sum.cpp

---
+++ Arrays/15._3Sum.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+        vector<vector<int>> res;
+
+        for(int i=0; i<n; i++){
+            if(i>0 && nums[i]==nums[i-1]) continue;
+
+            int l = i+1, r = n-1;
+            while(l<r){
+                int sum = nums[i] + nums[l] + nums[r];
+
+                if(sum==0){
+                    res.push_back({nums[i], nums[l], nums[r]});
+
+                    while(l < r && nums[l] == nums[l+1]) l++;
+                    while(l < r && nums[r] == nums[r-1]) r--;
+                    l++; 
+                    r--;
+                } 
+                else if(sum < 0){
+                    l++;
+                } 
+                else {
+                    r--;
+                }
+            }
+        }
+        return res;
+    }
+};
